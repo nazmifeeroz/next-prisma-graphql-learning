06d58f22d050f6711853e4bf5140cbd7475a9a1d
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2021-11-25 09:41:01 +0800

    :seedling: prisma seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+import { links as data } from '../data/links';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: 'testemail@gmail.com',
+      role: 'ADMIN',
+    },
+  });
+
+  await prisma.link.createMany({
+    data,
+  });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
